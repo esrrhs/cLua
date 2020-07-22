31b00a18fb8d85d80d0457f1bed77c6c556def9e
esrrhs <esrrhs@163.com>
2020-07-22 11:48:22 +0800

    add

--- clua.go
+++ clua.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"crypto/md5"
 	"encoding/binary"
+	"encoding/hex"
 	"flag"
 	"fmt"
 	"github.com/milochristiansen/lua/ast"
@@ -27,6 +29,7 @@ func main() {
 	showcode := flag.Bool("showcode", true, "show code")
 	showtotal := flag.Bool("showtotal", true, "show total")
 	showfunc := flag.Bool("showfunc", true, "show func")
+	lcovfile := flag.String("lcov", "", "output lcov info")
 
 	flag.Parse()
 
@@ -40,17 +43,25 @@ func main() {
 		return
 	}
 
+	err, lcovfd := check_lcovfile_begin(*lcovfile)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	if len(*filter) != 0 {
 		for _, p := range filedata {
 			if p.file == *filter {
-				calc(p, *showcode, *showtotal, *showfunc)
+				calc(p, *showcode, *showtotal, *showfunc, lcovfd)
 			}
 		}
 	} else {
 		for _, p := range filedata {
-			calc(p, *showcode, *showtotal, *showfunc)
+			calc(p, *showcode, *showtotal, *showfunc, lcovfd)
 		}
 	}
+
+	check_lcovfile_end(lcovfd)
 }
 
 func parse(filename string, root string) ([]FileData, bool) {
@@ -177,7 +188,264 @@ func (lv *luaVisitor) Visit(n ast.Node) ast.Visitor {
 	}
 }
 
-func calc(f FileData, showcode bool, showtotal bool, showfunc bool) {
+func do_showcode(f FileData, filecontent []string) {
+	fmt.Printf("coverage of %s:\n", f.path)
+
+	maxpre := uint64(0)
+	for _, c := range f.line {
+		if c > maxpre {
+			maxpre = c
+		}
+	}
+	pre := 0
+	for maxpre > 0 {
+		maxpre /= 10
+		pre++
+	}
+
+	for index, str := range filecontent {
+		val, ok := f.line[index+1]
+		if ok {
+			fmt.Printf(fmt.Sprintf("%%-%d", pre)+"v", val)
+		} else {
+			fmt.Printf(fmt.Sprintf("%%-%d", pre)+"v", " ")
+		}
+		fmt.Printf(" %s\n", strings.TrimRight(str, "\n"))
+	}
+}
+
+func do_showtotal(f FileData, filecontent []string, block []ast.Stmt) {
+	var funcdecs []*ast.FuncDecl
+	v := luaVisitor{f: func(n ast.Node) {
+		if n != nil {
+			switch nn := n.(type) {
+			case *ast.FuncDecl:
+				funcdecs = append(funcdecs, nn)
+			}
+		}
+	}}
+	for _, stmt := range block {
+		ast.Walk(&v, stmt)
+	}
+
+	linefound := 0
+	linehit := 0
+	for _, funcdec := range funcdecs {
+		line := funcdec.Line()
+
+		funcmaxline := 0
+		funcvalidline := make(map[int]int)
+		fv := luaVisitor{f: func(n ast.Node) {
+			funcvalidline[n.Line()]++
+			if n.Line() > funcmaxline {
+				funcmaxline = n.Line()
+			}
+		}}
+		for _, stmt := range funcdec.Block {
+			ast.Walk(&fv, stmt)
+		}
+
+		for i := line; i <= funcmaxline; i++ {
+			_, ok := f.line[i]
+			if ok {
+				_, ok = funcvalidline[i]
+				if ok {
+					linehit++
+				}
+			}
+			linefound++
+		}
+	}
+
+	if linehit != 0 {
+		fmt.Printf("%s total coverage %d%% %d/%d\n", f.path, linehit*100/linefound, linehit, linefound)
+	} else {
+		fmt.Printf("%s total coverage %d%% %d/%d\n", f.path, 0, linehit, 0)
+	}
+}
+
+func do_showfunc(f FileData, filecontent []string, block []ast.Stmt) {
+	var funcdecs []*ast.FuncDecl
+	v := luaVisitor{f: func(n ast.Node) {
+		if n != nil {
+			switch nn := n.(type) {
+			case *ast.FuncDecl:
+				funcdecs = append(funcdecs, nn)
+			}
+		}
+	}}
+	for _, stmt := range block {
+		ast.Walk(&v, stmt)
+	}
+
+	for _, funcdec := range funcdecs {
+		line := funcdec.Line()
+		funcname := filecontent[line-1]
+		funcname = strings.TrimSpace(funcname)
+		funcname = "[" + funcname + "]"
+
+		funcmaxline := 0
+		funcvalidline := make(map[int]int)
+		fv := luaVisitor{f: func(n ast.Node) {
+			funcvalidline[n.Line()]++
+			if n.Line() > funcmaxline {
+				funcmaxline = n.Line()
+			}
+		}}
+		for _, stmt := range funcdec.Block {
+			ast.Walk(&fv, stmt)
+		}
+
+		valid := 0
+		for i := line; i <= funcmaxline; i++ {
+			_, ok := f.line[i]
+			if ok {
+				_, ok = funcvalidline[i]
+				if ok {
+					valid++
+				}
+			}
+		}
+
+		if len(funcvalidline) != 0 {
+			fmt.Printf("%s function coverage %s %d%% %d/%d\n", f.path, funcname, valid*100/len(funcvalidline), valid, len(funcvalidline))
+		} else {
+			fmt.Printf("%s function coverage %s %d%% %d/%d\n", f.path, funcname, 0, valid, 0)
+		}
+	}
+}
+
+func do_lcovfile(f FileData, filecontent []string, block []ast.Stmt, lcovfd *os.File) {
+
+	lcovfd.WriteString(fmt.Sprintf("SF:%s\n", f.path))
+
+	var funcdecs []*ast.FuncDecl
+	v := luaVisitor{f: func(n ast.Node) {
+		if n != nil {
+			switch nn := n.(type) {
+			case *ast.FuncDecl:
+				funcdecs = append(funcdecs, nn)
+			}
+		}
+	}}
+	for _, stmt := range block {
+		ast.Walk(&v, stmt)
+	}
+
+	for _, funcdec := range funcdecs {
+		line := funcdec.Line()
+		funcname := filecontent[line-1]
+		funcname = strings.TrimSpace(funcname)
+		funcname = "[" + funcname + "]"
+
+		lcovfd.WriteString(fmt.Sprintf("FN:%d,%s\n", line, funcname))
+	}
+
+	funcfound := 0
+	funchit := 0
+	for _, funcdec := range funcdecs {
+		line := funcdec.Line()
+
+		funcname := filecontent[line-1]
+		funcname = strings.TrimSpace(funcname)
+		funcname = "[" + funcname + "]"
+
+		funcmaxline := 0
+		funcvalidline := make(map[int]int)
+		fv := luaVisitor{f: func(n ast.Node) {
+			funcvalidline[n.Line()]++
+			if n.Line() > funcmaxline {
+				funcmaxline = n.Line()
+			}
+		}}
+		for _, stmt := range funcdec.Block {
+			ast.Walk(&fv, stmt)
+		}
+
+		var total_valid uint64
+		for i := line; i <= funcmaxline; i++ {
+			value, ok := f.line[i]
+			if ok {
+				_, ok = funcvalidline[i]
+				if ok {
+					total_valid += value
+				}
+			}
+		}
+
+		if len(funcvalidline) != 0 {
+			lcovfd.WriteString(fmt.Sprintf("FNDA:%d,%s\n", total_valid, funcname))
+		} else {
+			lcovfd.WriteString(fmt.Sprintf("FNDA:%d,%s\n", 0, funcname))
+		}
+
+		funcfound++
+		if total_valid > 0 {
+			funchit++
+		}
+	}
+
+	lcovfd.WriteString(fmt.Sprintf("FNF:%d\n", funcfound))
+	lcovfd.WriteString(fmt.Sprintf("FNH:%d\n", funchit))
+
+	linefound := 0
+	linehit := 0
+	for _, funcdec := range funcdecs {
+		line := funcdec.Line()
+
+		funcmaxline := 0
+		funcvalidline := make(map[int]int)
+		fv := luaVisitor{f: func(n ast.Node) {
+			funcvalidline[n.Line()]++
+			if n.Line() > funcmaxline {
+				funcmaxline = n.Line()
+			}
+		}}
+		for _, stmt := range funcdec.Block {
+			ast.Walk(&fv, stmt)
+		}
+
+		for i := line; i <= funcmaxline; i++ {
+			value, ok := f.line[i]
+			if ok {
+				_, ok = funcvalidline[i]
+				if ok {
+					h := md5.New()
+					h.Write([]byte(filecontent[i]))
+					md5str := hex.EncodeToString(h.Sum(nil))
+					lcovfd.WriteString(fmt.Sprintf("DA:%d,%d,%s\n", i, value, md5str))
+					linehit++
+				}
+			}
+			linefound++
+		}
+	}
+
+	lcovfd.WriteString(fmt.Sprintf("LH:%d\n", linehit))
+	lcovfd.WriteString(fmt.Sprintf("LF:%d\n", linefound))
+
+	lcovfd.WriteString("end_of_record\n")
+}
+
+func check_lcovfile_begin(lcovfile string) (error, *os.File) {
+	if lcovfile != "" {
+		file, err := os.OpenFile(lcovfile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+		if err != nil {
+			return err, nil
+		}
+		file.WriteString("TN:\n")
+		return nil, file
+	}
+	return nil, nil
+}
+
+func check_lcovfile_end(lcovfd *os.File) {
+	if lcovfd != nil {
+		lcovfd.Close()
+	}
+}
+
+func calc(f FileData, showcode bool, showtotal bool, showfunc bool, lcovfd *os.File) {
 
 	filecontent, ok := readfile(f.path)
 	if !ok {
@@ -189,107 +457,20 @@ func calc(f FileData, showcode bool, showtotal bool, showfunc bool) {
 		return
 	}
 
-	validline := make(map[int]int)
-	v := luaVisitor{f: func(n ast.Node) {
-		validline[n.Line()]++
-	}}
-	for _, stmt := range block {
-		ast.Walk(&v, stmt)
-	}
-
 	if showcode {
-		fmt.Printf("coverage of %s:\n", f.path)
-
-		maxpre := uint64(0)
-		for _, c := range f.line {
-			if c > maxpre {
-				maxpre = c
-			}
-		}
-		pre := 0
-		for maxpre > 0 {
-			maxpre /= 10
-			pre++
-		}
-
-		for index, str := range filecontent {
-			val, ok := f.line[index+1]
-			if ok {
-				fmt.Printf(fmt.Sprintf("%%-%d", pre)+"v", val)
-			} else {
-				fmt.Printf(fmt.Sprintf("%%-%d", pre)+"v", " ")
-			}
-			fmt.Printf(" %s\n", strings.TrimRight(str, "\n"))
-		}
+		do_showcode(f, filecontent)
 	}
 
 	if showtotal {
-		valid := 0
-		for index, _ := range filecontent {
-			_, ok := f.line[index+1]
-			if ok {
-				_, ok = validline[index+1]
-				if ok {
-					valid++
-				}
-			}
-		}
-		if len(validline) != 0 {
-			fmt.Printf("%s total coverage %d%% %d/%d\n", f.path, valid*100/len(validline), valid, len(validline))
-		} else {
-			fmt.Printf("%s total coverage %d%% %d/%d\n", f.path, 0, valid, 0)
-		}
+		do_showtotal(f, filecontent, block)
 	}
 
 	if showfunc {
-		var funcdecs []*ast.FuncDecl
-		v := luaVisitor{f: func(n ast.Node) {
-			if n != nil {
-				switch nn := n.(type) {
-				case *ast.FuncDecl:
-					funcdecs = append(funcdecs, nn)
-				}
-			}
-		}}
-		for _, stmt := range block {
-			ast.Walk(&v, stmt)
-		}
+		do_showfunc(f, filecontent, block)
+	}
 
-		for _, funcdec := range funcdecs {
-			line := funcdec.Line()
-			funcname := filecontent[line-1]
-			funcname = strings.TrimSpace(funcname)
-			funcname = "[" + funcname + "]"
-
-			funcmaxline := 0
-			funcvalidline := make(map[int]int)
-			fv := luaVisitor{f: func(n ast.Node) {
-				funcvalidline[n.Line()]++
-				if n.Line() > funcmaxline {
-					funcmaxline = n.Line()
-				}
-			}}
-			for _, stmt := range funcdec.Block {
-				ast.Walk(&fv, stmt)
-			}
-
-			valid := 0
-			for i := line; i <= funcmaxline; i++ {
-				_, ok := f.line[i]
-				if ok {
-					_, ok = funcvalidline[i]
-					if ok {
-						valid++
-					}
-				}
-			}
-
-			if len(funcvalidline) != 0 {
-				fmt.Printf("%s function coverage %s %d%% %d/%d\n", f.path, funcname, valid*100/len(funcvalidline), valid, len(funcvalidline))
-			} else {
-				fmt.Printf("%s function coverage %s %d%% %d/%d\n", f.path, funcname, 0, valid, 0)
-			}
-		}
+	if lcovfd != nil {
+		do_lcovfile(f, filecontent, block, lcovfd)
 	}
 }
 
